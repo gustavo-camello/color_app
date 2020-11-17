40c8b2a11caa2c0975689de0d8a773c3885b85e3
Gustavo Camello <gucamello@gmail.com>
2020-11-17 13:25:43 +0000

    adjust text colors

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import CopyToClipBoard from 'react-copy-to-clipboard';
 import {Link} from 'react-router-dom';
+import chroma from 'chroma-js';
 import './ColorBox.css';
 
 class ColorBox extends Component {
@@ -23,6 +24,9 @@ class ColorBox extends Component {
     render () {
         const {name, background, id, paletteId, showLink} = this.props;
         const {copied} = this.state;
+        const isDarkColor = chroma(background).luminance() <= 0.08;
+        const isLightColor = chroma(background).luminance() >= 0.07;
+
         return (
             <CopyToClipBoard text={background} onCopy={this.changeCopystate}>
                 <div style={{ background: background}} className="ColorBox">
@@ -32,17 +36,17 @@ class ColorBox extends Component {
                     />
                     <div className={`copy-message ${copied && 'show'}`}>
                         <h1>Copied!</h1>
-                        <p>{this.props.background}</p>
+                        <p className={isLightColor && "dark-text"}>{this.props.background}</p>
                     </div>
                     <div className="copy-container">
                         <div className="box-content">
-                            <span>{name}</span>
+                            <span className={isDarkColor && "light-text"}>{name}</span>
                         </div>
-                        <button className="copy-button">Copy</button>
+                        <button className={`copy-button ${isLightColor && "dark-text"}`}>Copy</button>
                     </div>
                     {showLink && (
                         <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()}>
-                            <span className="see-more">More</span>
+                            <span className={`see-more ${isLightColor && "dark-text"}`}>MORE</span>
                         </Link>
                     )}
                 </div>
